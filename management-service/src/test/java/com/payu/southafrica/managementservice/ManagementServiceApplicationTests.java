01b860df2e25e251d51b09649ac6655622ec6f71
Malesela Sithole <malesela@hyvemobile.co.za>
2023-06-24 19:06:06 +0200

    init management-service

---
+++ management-service/src/test/java/com/payu/southafrica/managementservice/ManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payu.southafrica.managementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
